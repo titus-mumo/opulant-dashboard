5b879728fbc9e47f2bf4defd5eca3dc3908d215f
titus-mumo <tituskennedy74@gmail.com>
2023-09-29 14:35:09 +0300

    changed package.json

--- src/index.js
+++ src/index.js
@@ -4,26 +4,16 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
-import { transitions, positions, Provider as AlertProvider } from 'react-alert';
-import AlertTemplate from 'react-alert-template-basic'
 
-const options = {
-  // you can also just use 'bottom center'
-  position: positions.BOTTOM_CENTER,
-  timeout: 5000,
-  offset: '30px',
-  // you can also just use 'scale'
-  transition: transitions.SCALE
-}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <AlertProvider template={AlertTemplate} {...options}>
         <ScrollToTop />
         <App />
-      </AlertProvider>
     </Router>
   </React.StrictMode>
 );
